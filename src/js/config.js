2eb148c9eaebb20de7dcb862516f317ccc6734ba
Priyanshu kumawat <88786642+kumarsonsoff3@users.noreply.github.com>
2023-12-18 00:32:46 +0530

    Initial Commit!

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6071223d-98dd-43b2-8f43-c54225fde2c2';
+export const MODAL_CLOSE_SEC = 2.5;
